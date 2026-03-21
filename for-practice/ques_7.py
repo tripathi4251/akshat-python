dfd94dd2399b69ae95af02b6a847487f900d4a60
tripathi4251 <tripathi.4251@gmail.com>
2026-03-22 00:19:16 +0530

    add easy questions

---
+++ for-practice/ques_7.py
@@ -0,0 +1,4 @@
+#print cubes of numbers from 1 to 5
+
+for i in range(1,6):
+    print(i*i*i)
---
+++ for-practice/ques_8.py
@@ -0,0 +1,5 @@
+#print numbers from 1 to n
+
+n=int(input("enter a number"))
+for i in range(1,n+1):
+       print(i)
