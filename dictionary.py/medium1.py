214f1b02ccf3b184fe837dd4b1544eac9fceb039
tripathi4251 <tripathi.4251@gmail.com>
2026-03-24 01:31:20 +0530

    added medium dictionary coding questions

---
+++ dictionary.py/medium1.py
@@ -0,0 +1,13 @@
+dict1= { 
+    "brand": "ford",
+    "color": "red",
+}
+
+dict2 = {
+    "year": 1964,
+    "model": "mustang",
+}
+
+dict1.update(dict2)
+
+print(dict1)
---
+++ dictionary.py/ques7.py
@@ -0,0 +1,9 @@
+#Loop through dictionary  Use a loop to print both keys and values one by one.#
+
+dictA = {
+    "fruit": "lemon",
+    "vegetable": "carrot",
+}
+
+for k,v in dictA.items():
+    print(k,v)
