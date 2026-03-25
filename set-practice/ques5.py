7c1db10e9a78d0f9a300e639c8a54e19a40173ca
tripathi4251 <tripathi.4251@gmail.com>
2026-03-26 00:09:34 +0530

    aaded set easy coding questions

---
+++ set-practice/ques5.py
@@ -0,0 +1,10 @@
+#create a copy of a set and print both sets..
+
+s8={"hello","world","python","java"}
+collection=set()
+collection.add("hello")
+collection.add("world")
+collection.add("python")
+collection.add("java")
+print(collection)
+print(s8)
