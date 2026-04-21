7b1df94b5d559a6813db22439793944c13034f75
tripathi4251 <tripathi.4251@gmail.com>
2026-04-21 20:02:39 +0530

    add easy coding question of functions

---
+++ functions_practice/ques4.py
@@ -0,0 +1,10 @@
+#maximum of two numbers 
+#return the greater of two numbers
+
+def greater_num(a,b):
+    if a>b:
+        return ("a is greater than b")
+    elif b>a:
+        return "b is greater than a"
+    
+print(greater_num(9,6))
