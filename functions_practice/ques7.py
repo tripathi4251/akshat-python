8fc8d674cfcb1e30ab154b93f24e645c2d6daa1c
tripathi4251 <tripathi.4251@gmail.com>
2026-04-22 00:00:24 +0530

    add easy coding question of function

---
+++ functions_practice/ques7.py
@@ -0,0 +1,17 @@
+#multiply two numbers
+#return multiplication of two numbers
+
+def multiply(a,b):
+    return a*b
+print(multiply(9,5))
+
+
+
+#check if a number is divisible or not by 5 and 11
+def check(num):
+    if num%5==0 and num%11==0:
+        return "it is divisible"
+    else:
+      return  "it is not divisible"
+
+print(check(11))
