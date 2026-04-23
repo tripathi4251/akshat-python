65c5306cef2d82ae7c596a0b8a9aedbdae4e0d2c
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 23:49:14 +0530

    add medium coding question of variables

---
+++ variables_practice/medium_ques3.py
@@ -0,0 +1,9 @@
+#Username Generator
+# What to do:
+#Take first name + birth year
+#Create username
+
+name=input("enter firstname")
+birthyear=input("enter your birthyear")
+username=name+birthyear
+print(username)
