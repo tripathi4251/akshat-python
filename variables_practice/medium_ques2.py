ddb8454c1050f5ac1048ee3ed5aef5c718b6a7d1
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 22:15:43 +0530

    add medium coding question of variables

---
+++ variables_practice/medium_ques2.py
@@ -0,0 +1,12 @@
+#Temperature Converter (Both Ways)
+# What to do:
+#Convert Celsius → Fahrenheit AND vice versa
+
+
+celsius=30
+fahrenheit=50
+
+calculate1=(celsius*9/5)+35
+calculate2=(fahrenheit-32)*5/9
+print(" fahrenheit :" , calculate1)
+print(" celsius :" , calculate2)
