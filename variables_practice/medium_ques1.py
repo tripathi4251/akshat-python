3f091296b278494b7e875fe2bf76d8b5f9ac0394
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 21:46:50 +0530

    add medium coding question of variable

---
+++ variables_practice/medium_ques1.py
@@ -0,0 +1,24 @@
+#Even or Odd Checker
+# What to do:
+#Store a number
+#Check if divisible by 2
+#Print result
+
+num=5
+if num%2==0:
+    print("even")
+else: 
+    print("odd")
+
+
+#Largest of Two Numbers
+# What to do:
+#Store two numbers
+#Compare and print largest
+
+a=5
+b=10
+if a>b:
+    print(a)
+else:
+    print(b)
