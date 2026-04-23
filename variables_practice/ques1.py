c71a50c6c41b50f059a766ea180116c8939fc28d
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 20:50:34 +0530

    add easy coding questions of variables

---
+++ variables_practice/ques1.py
@@ -0,0 +1,19 @@
+
+#Add Two Numbers
+#Create two variables and print their sum.
+
+a=5
+b=6
+sum=a+b
+print(sum)
+
+
+#Simple Interest
+#Variables: principal, rate, time
+#Calculate SI
+
+
+p=25
+r=0.05
+t=3
+print("SI:", p*r*t)
