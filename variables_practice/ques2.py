0f3e9b03f19a60fc8ea7d914c193f6cb693c9aae
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 21:05:53 +0530

    add easy coding question

---
+++ variables_practice/ques2.py
@@ -0,0 +1,13 @@
+#Input + Print
+#Take user input and print it
+a=input("enter a name:")
+print(a)
+
+
+#Rectangle Area
+#Store length and width
+#Print area
+
+length=34
+width=6
+print("area of rectangle is:" , length*width)
