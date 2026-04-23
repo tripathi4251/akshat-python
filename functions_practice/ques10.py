e88c946695dc7c6d86872abe0e2b1bec36e6b73c
tripathi4251 <tripathi.4251@gmail.com>
2026-04-23 17:25:54 +0530

    add easy question of fumctions

--- functions_practice/ques10.py
+++ functions_practice/ques10.py
@@ -1,8 +1,21 @@
 #Check Palindrome
 # Check if string reads same forward/backward.
 
-
 def is_palindrome(s):
  return s==s[::-1]
 
 print(is_palindrome("madam"))
+
+
+#Count Vowels
+# Count vowels in string.
+
+def count_vowels(s):
+    vowels="aeiouAEIOU"
+    count=0
+    for ch in s:
+        if ch in vowels:
+               count+=1
+               return count
+    
+print(count_vowels("hello world"))
