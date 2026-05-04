e43ecdac04161e0749bb4752e17414b312849396
tripathi4251 <tripathi.4251@gmail.com>
2026-05-05 00:37:29 +0530

    add easy coding question of string

--- dictionary-practice/string.py/ques4.py
+++ dictionary-practice/string.py/ques4.py
@@ -11,3 +11,13 @@ str5="akshat"
 print(str5[0])
 
 
+#Find last character
+str6="tripathi"
+print(str6[-1])
+
+
+#Count occurrence of a character
+# "banana" → a = 3
+
+str1="banana"
+print(str1.count("a"))
