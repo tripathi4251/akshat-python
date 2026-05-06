2c59dc47534118b4eb46d95a010a01cace99cba6
tripathi4251 <tripathi.4251@gmail.com>
2026-05-06 13:46:53 +0530

    add medium coding question of for loop

---
+++ loops_practice/for-practice/medium_ques1.py
@@ -0,0 +1,7 @@
+
+ #Print the square of each number from 1 to n.
+
+n=11
+for i in range(1,n+1):
+    print(i*i)
+
--- loops_practice/for-practice/ques_8.py
+++ loops_practice/for-practice/ques_8.py
@@ -3,3 +3,5 @@
 n=int(input("enter a number"))
 for i in range(1,n+1):
        print(i)
+
+       
